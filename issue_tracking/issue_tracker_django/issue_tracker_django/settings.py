bba4463a2201f8b2c834caa5f62b25c37938bcc7
AisAis2 <128880904+AisAis2@users.noreply.github.com>
2024-03-09 16:59:12 +0100

    update

--- issue_tracking/issue_tracker_django/issue_tracker_django/settings.py
+++ issue_tracking/issue_tracker_django/issue_tracker_django/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4gtd9cuvr+fqj=61g6h9e_s4x(bbinja7@k+e6)xzc%a&e%9z2'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
